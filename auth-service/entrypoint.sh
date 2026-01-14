fcd381f5110c4c70b4ec792efed4752fba48c95f
szerw <szerwinskimateusz97@gmail.com>
2026-01-14 21:25:57 +0100

    Fix Cloud Run startup (use PORT env var)

--- auth-service/entrypoint.sh
+++ auth-service/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -e
 
 echo "Running migrations..."
 python manage.py migrate --noinput
 
-echo "Starting Gunicorn..."
-gunicorn auth_service.wsgi:application --bind 0.0.0.0:8080
+echo "Starting Gunicorn on port $PORT"
+gunicorn auth_service.wsgi:application --bind 0.0.0.0:$PORT
